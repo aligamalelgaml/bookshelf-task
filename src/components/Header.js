152a9dafae39dd54548e70ce4ad8e13cdedd1017
aligamalelgaml <ali.gamal.elgaml@gmail.com>
2023-06-08 21:48:08 +0300

    removed unneeded files, started work on project, added header

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Container, Row } from 'react-bootstrap';
+
+
+
+class Header extends React.Component {
+
+  render() {
+    return (
+      <header className='header'>
+      <Container>
+        <Row className='text-center pt-4'>
+          <h1 className='fw-bold text-white'>MyReads</h1>
+        </Row>
+      </Container>
+      </header>
+    );
+  }
+}
+
+
+export default Header;
+
