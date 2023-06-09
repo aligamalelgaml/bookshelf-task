8c083679f2ab9197ef4cd3e28addf0ee08a325cc
aligamalelgaml <ali.gamal.elgaml@gmail.com>
2023-06-09 10:00:06 +0300

    added deboucing to API calls to prevent API call on every search input keystroke, added localstorage setting of selected books. WIP: update Library component to reflect changes to localstorage books.

--- src/components/SearchBook.js
+++ src/components/SearchBook.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Modal } from 'react-bootstrap';
+import { Button, Modal, Card, Row, Col } from 'react-bootstrap';
 import axios from 'axios';
 
 // TODO: Debounce bookSearch method, format results into cards, save selected books to localstorage.
@@ -8,9 +8,11 @@ import axios from 'axios';
 class SearchBook extends React.Component {
 
     state = { showModal: false, results: [], loading: false };
+    targetRefs = [];
 
-    fetchBooks(title) {
-        axios.get(`https://openlibrary.org/search.json?q=${title}`).then(results => this.setState({ results: results.data.docs })).finally(() => this.setState({ loading: false }));;
+    fetchBooks = (title) => {
+        console.log("Fetching data from API..");
+        axios.get(`https://openlibrary.org/search.json?q=${title}`).then(results => this.setState({ results: results.data.docs, loading: false }));
     }
 
     toggleModal = () => {
@@ -19,11 +21,28 @@ class SearchBook extends React.Component {
         }));
     };
 
-
-    bookSearch = (e) => {
+    debouncedBookSearch = (e) => {
         this.setState({loading: true});
-        this.fetchBooks(e.target.value);
+        clearTimeout(this.timeout);
+
+        const debouncedSearch = this.__debounce(this.fetchBooks);
+        debouncedSearch(e.target.value);
     }
+
+    __debounce(callback, delay = 2000) {
+        return (...args) => {
+            this.timeout = setTimeout(() => {callback(...args)}, delay);
+        }
+    }
+
+    addBook = (book) => {
+        console.log("Adding.. + " + book.title)
+        const bookList = localStorage.getItem("currentBooks") ? JSON.parse(localStorage.getItem("currentBooks")) : [];
+        bookList.push(book);
+        localStorage.setItem("currentBooks", JSON.stringify(bookList))
+    }
+
+
 
 
     render() {
@@ -40,23 +59,47 @@ class SearchBook extends React.Component {
                     <Modal.Header closeButton>
                         <Modal.Title>Search Books</Modal.Title>
                     </Modal.Header>
-                    <Modal.Body>
-                        <input type="text" onChange={this.bookSearch} />
 
-                        <div className='bookResults text-center'>
-                            {this.state.loading ? (
-                                <p>Loading...</p>
-                            ) : this.state.results && this.state.results.length > 0 ? (
-                                this.state.results.map(book => (
-                                    <div key={book.key}>
-                                        <h4>{book.title}</h4>
-                                        <p>{book.author_name}</p>
-                                    </div>
-                                ))
-                            ) : (
-                                <p>No results found.</p>
-                            )}
-                        </div>
+                    <Modal.Body>
+                        <input type="text" onChange={this.debouncedBookSearch} />
+
+                        <Row className='mt-4'>
+                                {this.state.loading ? (
+                                    <span>Loading...</span>
+                                ) : this.state.results && this.state.results.length > 0 ? (
+                                    this.state.results.map(book => (
+                                        <Col xs={2} key={book.key} className='mt-5'>
+                                            <Card className='h-100'>
+                                                <Card.Img variant="top" src={`https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`}/>
+                                                <Card.Body>
+                                                    <Card.Text>
+                                                        <span className='fs-5 fw-bold'>{book.title}</span>
+
+                                                        <div>
+                                                            {book.author_name ? (
+                                                                book.author_name.map((author, index) => {
+                                                                    return <span key={index}>{author},</span>;
+                                                                })
+                                                            ) : (
+                                                                <p>N/A</p>
+                                                            )}
+                                                        </div>
+                                                    </Card.Text>
+                                                </Card.Body>
+                                                <Card.Footer>
+                                                    <Button key={book.key} className='rounded-circle' variant="primary" onClick={this.addBook.bind(this, book)}>
+                                                        +
+                                                    </Button>
+                                                </Card.Footer>
+                                            </Card>
+                                        </Col>
+                                    ))
+                                ) : (
+                                    <span>No results found.</span>
+                                )}
+                        </Row>
+
+
                     </Modal.Body>
                 </Modal>
             </>
